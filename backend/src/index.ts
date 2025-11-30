91dda9d5813e5dbd70d342efd5acb1fbd78753ad
vRok-Emil <emil.ronnqvist@chasacademy.se>
2025-11-30 16:25:53 +0100

    Lagt till att en email måste ha "@", att ett lösenord måste vara åtta tecken långt (minst), att man kan hämta alla användare, uppdatera användare, ta bort en användare och

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,22 +1,30 @@
-import express, { Express, Request, Response, Router } from 'express';
+import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { connectDB } from './config/database';
-import { createUser } from './routes/userRoutes';
+import { 
+      createUser, 
+      getUserById,
+      getAllUsers,
+      deleteUser,
+      updateUser
+} from './routes/userRoutes';
 
 dotenv.config();
-const router = express.Router();
+
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 // Connect to MongoDB
 connectDB();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
-router.get("/", createUser);
-
-
+app.post("/signup", createUser);
+app.get("/users", getAllUsers);
+app.get("/users/:id", getUserById);
+app.delete("/users/:id", deleteUser);
+app.put("/users/:id", updateUser); // Changed from app.update to app.put
 
 // Start server
 app.listen(PORT, () => {
